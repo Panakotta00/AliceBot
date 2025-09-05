afe95b21f6febe0f170bba6fc11fe4b216c7d7fc
Panakotta00 <panakotta00@gmail.com>
2025-09-05 19:49:15 +0200

    fix: change symlog base

--- metrics.go
+++ metrics.go
@@ -61,7 +61,7 @@ func barChart(entries *[]int64, title string) (*plot.Plot, error) {
 	p.HideX()
 
 	p.Y.Min = 0
-	p.Y.Scale = SymlogScale{Base: 2, LinScale: 1, LinThresh: 10}
+	p.Y.Scale = SymlogScale{Base: 2, LinScale: 1, LinThresh: 20}
 	//p.Y.Tick.Marker = SymlogTicks{Base: 10, LinThresh: 10}
 	p.Y.Label.Text = "Msgs./Day"
 	p.Y.Label.TextStyle.Color = color.White
